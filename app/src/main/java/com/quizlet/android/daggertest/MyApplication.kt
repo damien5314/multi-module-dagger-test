a24e4fb32a5e46a2dc01fd62152df77267a83ef7
Damien Diehl <damien5314@gmail.com>
2019-06-17 15:55:44 -0700

    initial commit

---
+++ app/src/main/java/com/quizlet/android/daggertest/MyApplication.kt
@@ -0,0 +1,23 @@
+package com.quizlet.android.daggertest
+
+import android.app.Activity
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
+
+class MyApplication : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+
+    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+
+    override fun onCreate() {
+        super.onCreate()
+        // Hoping we don't need to do this now-- ActivityInjector should provide everything
+//        component = buildDaggerGraph()
+//        component.inject(this)
+    }
+}
